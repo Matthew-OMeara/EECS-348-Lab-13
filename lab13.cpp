86305af3a60955bfdb9f66c4f3d07ebc7e533fcc
Matthew-OMeara <157535325+Matthew-OMeara@users.noreply.github.com>
2025-11-30 13:11:11 -0600

    Updated to fulfill clarifications

--- lab13.cpp
+++ lab13.cpp
@@ -1,31 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-bool fail_lecture(const vector<int>& attendance_records) {
-  int absent_count = 0;
-  for (int i = 1; i < attendance_records.size(); ++i) {
-    absent_count += attendance_records[i] == 0;
-  }
-  return absent_count >= 3;
-}
-
-int main() {
-  vector<int> question3 = {1,1,1,1,1,1,1,1,1,1};
-  cout << "Test case that executes fault but does not enter error state: " << "{1,1,1,1,1,1,1,1,1,1}" << endl;
-  cout << "Result: " << fail_lecture(question3) << "\n" << endl;
-  
-  vector<int> question4 = {0,1,0,0,0,1,1,1,1,1};
-  cout << "Test case that results in error state but not a failure: {0,1,0,0,0,1,1,1,1,1}" << endl;
-  cout << "Result: " << fail_lecture(question4) << "\n" << endl;
-  
-  vector<int> question5 = {0,1,0,0,1,1,1,1,1,1};
-  cout << "Test case that results in failure: {0,1,0,0,1,1,1,1,1,1}" << endl;
-  cout << "Result: " << fail_lecture(question5) << "\n" << endl;
-}
-
-
 /* Answers to questions:
 
 1. Is there a fault in the program? If so, locate it and explain.
@@ -54,3 +26,42 @@ int main() {
   expected output: 1 (True)
   actual output: 0 (False)
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool fail_lecture(const vector<int>& attendance_records) {
+  int absent_count = 0;
+  for (int i = 1; i < attendance_records.size(); ++i) {
+    absent_count += attendance_records[i] == 0;
+  }
+  return absent_count >= 3;
+}
+
+// This is the corrected version of fail_lecture
+bool corrected_fail_lecture(const vector<int>& attendance_records) {
+  int absent_count = 0;
+  for (int i = 0; i < attendance_records.size(); ++i) {
+    absent_count += attendance_records[i] == 0;
+  }
+  return absent_count >= 3;
+}
+
+int main() {
+  vector<int> question3 = {1,1,1,1,1,1,1,1,1,1};
+  cout << "Test case that executes fault but does not enter error state: " << "{1,1,1,1,1,1,1,1,1,1}" << endl;
+  cout << "Expected Result: " << corrected_fail_lecture(question3) << endl;
+  cout << "Actual Result: " << fail_lecture(question3) << "\n" << endl;
+  
+  vector<int> question4 = {0,1,0,0,0,1,1,1,1,1};
+  cout << "Test case that results in error state but not a failure: {0,1,0,0,0,1,1,1,1,1}" << endl;
+  cout << "Expected Result: " << corrected_fail_lecture(question4) << endl;
+  cout << "Actual Result: " << fail_lecture(question4) << "\n" << endl;
+  
+  vector<int> question5 = {0,1,0,0,1,1,1,1,1,1};
+  cout << "Test case that results in failure: {0,1,0,0,1,1,1,1,1,1}" << endl;
+  cout << "Expected Result: " << corrected_fail_lecture(question5) << endl;
+  cout << "Actual Result: " << fail_lecture(question5) << "\n" << endl;
+}
